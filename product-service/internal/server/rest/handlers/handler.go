2f09a7f4b9777a16039bcf6fd6c90324c763b1c1
Joshdike <dikejoshua@gmail.com>
2025-11-25 19:09:13 +0300

    server and handlers updated (#1)
    * saving progress: server & handler
    
    * quick fix: server

--- product-service/internal/server/rest/handlers/handler.go
+++ product-service/internal/server/rest/handlers/handler.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,4 +20,50 @@ func (handler *ProductHandler) GetProducts(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "all products here",
 	})
+}
+
+func (handler *ProductHandler) GetProductByID(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid id",
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("product with id: %d", id),
+	})
+}
+
+func (handler *ProductHandler) CreateProduct(c *gin.Context) {
+	//A struct will hold the new product
+	//bind the info in request to the struct, save to the database and return response
+
+	c.JSON(http.StatusAccepted, gin.H{
+		"message": "product created successfully",
+	})
+}
+
+func (handler *ProductHandler) UpdateProduct(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid id",
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("product %d updated", id),
+	})
+}
+
+func (handler *ProductHandler) DeleteProduct(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid id",
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("product %d deleted", id),
+	})
+
 }
