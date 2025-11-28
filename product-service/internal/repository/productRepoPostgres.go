eaf8c88f7248acdb27108c046162c534e218ea7a
shubham_dixit <shubam.dixit@janio.asia>
2025-11-28 22:35:18 +0530

    product service

--- product-service/internal/repository/productRepoPostgres.go
+++ product-service/internal/repository/productRepoPostgres.go
@@ -12,15 +12,12 @@ type ProductRepositoryPostgres struct {
 
 func (p *ProductRepositoryPostgres) CreateProduct(product models.Product) (int, error) {
 	// We will write the sql query
-	exec, err := p.db.DB.Exec("INSERT INTO products(name,price,description) VALUES ($1,$2,$3) RETURNING id", product.Name, product.Price, product.Description)
+	_, err := p.db.DB.Exec("INSERT INTO products(name,price,description) VALUES ($1,$2,$3) RETURNING id", product.Name, product.Price, product.Description)
 	if err != nil {
 		return 0, err
 	}
-	id, err := exec.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-	return int(id), nil
+
+	return 0, nil
 }
 
 func (p *ProductRepositoryPostgres) GetProductByID() {
