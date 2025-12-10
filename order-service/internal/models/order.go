cd8c03f93db4ac0b557a8cb5149575f15ab1f2cf
Joshdike <dikejoshua@gmail.com>
2025-12-11 00:35:35 +0300

    order rest Api implemented

--- order-service/internal/models/order.go
+++ order-service/internal/models/order.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type Order struct {
-	ProductName        string
-	ProductDescription string
-	ProductQuantity    int
-	Address            string
+	ID                 string    `json:"id" bson:"_id,omitempty"`
+	ProductName        string    `json:"product_name" bson:"product_name"`
+	ProductDescription string    `json:"product_description" bson:"product_description"`
+	ProductQuantity    int       `json:"product_quantity" bson:"product_quantity"`
+	Address            string    `json:"address" bson:"address"`
+	CreatedAt          time.Time `json:"created_at" bson:"created_at"`
 }
--- order-service/internal/repository/order.go
+++ order-service/internal/repository/order.go
@@ -1,4 +1,9 @@
 package repository
 
+import "order-service/internal/models"
+
 type OrderRepository interface {
+	CreateOrder(order models.Order) (string, error)
+	GetOrder(id string) (models.Order, error)
+	DeleteOrder(id string) error
 }
