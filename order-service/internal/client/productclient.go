f58630b37d18432627b30b80723eea56d0e9de30
Joshdike <dikejoshua@gmail.com>
2025-12-18 18:32:58 +0300

    product grpc client

--- order-service/internal/client/productclient.go
+++ order-service/internal/client/productclient.go
@@ -1,15 +1,44 @@
 package client
 
 import (
-	productServiceGrpc "order-service/internal/client/proto/v1"
+	"context"
+	v1 "order-service/internal/client/proto/v1"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type ProductGrpcClient struct {
-	conn          grpc.ClientConn
-	productclient productServiceGrpc.ProductServiceClient
+	conn          *grpc.ClientConn
+	productclient v1.ProductServiceClient
 }
 
 // you can write a method invoke the server
 
+func NewProductClient(address string) (*ProductGrpcClient, error) {
+	conn, err := grpc.NewClient(address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	client := v1.NewProductServiceClient(conn)
+
+	return &ProductGrpcClient{
+		conn:          conn,
+		productclient: client,
+	}, nil
+
+}
+
+func (c *ProductGrpcClient) GetProductById(ctx context.Context, productID int64) (*v1.ProductResponse, error) {
+	req := &v1.ProductRequest{
+		ProductId: productID,
+	}
+
+	return c.productclient.GetProductById(ctx, req)
+}
+
+func (c *ProductGrpcClient) Close() error {
+	return c.conn.Close()
+}
